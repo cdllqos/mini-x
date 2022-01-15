48e1ff4ec2917480c744117f8d5f153ac67d8ef4
Mystic <cdllqos@outlook.com>
2022-01-15 21:37:58 +0800

    refactor: copy whole third component files (#1)

--- src/plugins/json.plugin.ts
+++ src/plugins/json.plugin.ts
@@ -1,11 +1,11 @@
+import { copyLib, getStaticTarget } from '@src/utils/fname.util';
+import { hasCopiedLib, setCopiedLib } from '@src/state/js.state';
+
 import { BasePlugin } from './base.plugin';
-import { NODE_MODULES } from '@src/constrants';
 import { Watcher } from '@src/core/watcher';
 import { WorkspaceFile } from '@src/enum/workspace-file';
 import { buildFileExtMatcher } from '@src/utils/plugin.util';
 import { fsUtil } from '@src/utils/file.util';
-import { getStaticTarget } from '@src/utils/fname.util';
-import { pathProxy } from '@src/utils/path.util';
 
 export class JsonPlugin extends BasePlugin {
   matcher = buildFileExtMatcher(WorkspaceFile.json);
@@ -32,45 +32,17 @@ export class JsonPlugin extends BasePlugin {
     const paths = Object.values(usingComponents);
     paths.forEach((p) => {
       if (p.startsWith('.')) {
-        if (fname.includes(NODE_MODULES)) {
-          const basePath = pathProxy.resolve(pathProxy.dirname(fname), p);
-          this.componentChange(basePath, watcher);
-        }
         return;
       }
 
       const splits = p.split('/');
       const packageName = splits[0];
-      const packageJsonPath = pathProxy.resolve(
-        NODE_MODULES,
-        packageName,
-        'package.json',
-      );
-      const { miniprogram } = require(packageJsonPath) as {
-        miniprogram: string;
-      };
-
-      if (!miniprogram) {
-        throw new Error(`can't find miniprogram is path:`);
+      if (hasCopiedLib(packageName)) {
+        return true;
       }
 
-      splits.splice(1, 0, miniprogram);
-      const relativePath = splits.join('/');
-      const basePath = pathProxy.resolve(NODE_MODULES, relativePath);
-      this.componentChange(basePath, watcher);
-    });
-  }
-
-  private componentChange(basePath: string, watcher: Watcher) {
-    const fileTypes = [
-      WorkspaceFile.js,
-      WorkspaceFile.json,
-      WorkspaceFile.wxml,
-      WorkspaceFile.wxss,
-    ];
-    fileTypes.forEach((fileExt) => {
-      const filePath = `${basePath}.${fileExt}`;
-      watcher.fileChange(filePath);
+      copyLib(packageName);
+      setCopiedLib(packageName);
     });
   }
 }
